d1da95e1d5db24f043e8863d03862866213f9e8b
TamalC <tamal.chakraborty885@gmail.com>
2021-07-06 18:36:50 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,13 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-             Fib Calculator
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
