059c5ce06f2344db41f1c6ee84aca9c5955de203
TamalC <tamal.chakraborty885@gmail.com>
2021-07-10 15:13:48 -0500

    added -t before sha tag

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t tchakrab/multi-client:latest tchakrab/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t tchakrab/multi-server:latest tchakrab/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t tchakrab/multi-worker:latest tchakrab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t tchakrab/multi-client:latest -t tchakrab/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tchakrab/multi-server:latest -t tchakrab/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t tchakrab/multi-worker:latest -t tchakrab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push tchakrab/multi-client:latest
 docker push tchakrab/multi-server:latest
 docker push tchakrab/multi-worker:latest
